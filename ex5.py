aa73bc8a91a6dda355eec51d5be28c3d974f5736
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-03-20 17:02:37 -0600

    Completes exercise 5 of learn python the hard way.

---
+++ ex5.py
@@ -0,0 +1,23 @@
+name = 'Zed A. Shaw'
+age = 35
+height = 74
+weight = 180
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print(f"Let's talk about {name}. ")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
+print(f"Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on coffee.")
+
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
+
+kilos = weight * 0.45359237
+print(f"He weighs {kilos} kilos.")
+
+centimeters = height * 2.54
+print(f"He is {centimeters} centimeters tall.")
