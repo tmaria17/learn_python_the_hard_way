74a34fdc3d7c4cc02b036261a59dc93157a699c8
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-03-19 22:42:16 -0600

    Solutions to exercise one and exercise 2 of learn python the hard way.

---
+++ ex3.py
@@ -0,0 +1,24 @@
+# This line of code prints this string.
+print("I will now count my chickens:")
+# This line of code prints the srting hens and does the math to tell you how much hens you have.
+print("Hens", 25 + 30 /6)
+# This line of code prints the number of roosters
+print("Roosters", 100 - 25 * 3 % 4)
+# This line of code prints the string
+print("Now I will count the eggs: ")
+# This line of code calculates and prints the number of egg
+print(3 + 2 + 1 - 5 +4%2 -1 /4+6)
+#This line of code prints this string.
+print("Is it true that 3 + 2 < 5-7?")
+#this line of code returns false
+print(3+2 < 5 -7)
+#This line of code prints out the question and answer
+print("What is 3 + 2?", 3+2)
+#This line of code prints out the question and answer
+print("What is 5 - 7?", 5 - 7)
+# This line of code prints out a string
+print("How about some more.")
+#Thus line of code prints out the question and the answer(does the math)
+print("Is it greater?", 5 >= -2)
+#Thus line of code prints out the question and the answer (does the math)
+print("Is it greater or equal?", 5<= -2)
