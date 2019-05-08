9c88955a1c1f98963202cb353ac7cae57655708e
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-05-07 22:20:34 -0600

    Complete exercise 11.

---
+++ ex11.py
@@ -0,0 +1,13 @@
+print("How old are you?", end='')
+age = input()
+print("How tall are you?", end= '')
+height = input()
+print("How much do you weigh?", end = '')
+weight = input()
+print(f"So you're {age} old, {height} tall and {weight} heavy.")
+
+print("What is your cat's name?", end='')
+
+cat = input()
+
+print(f"And your cat's name is {cat}")
